7e9c181c1cae09deb811a5576f88b1ff694c9167
EC2 Default User <ec2-user@ip-172-31-7-44.ec2.internal>
2023-05-26 18:00:04 +0000

    added kenkinsfile

--- hello-world.py
+++ hello-world.py
@@ -1,11 +1,1 @@
-pipeline {
-    agent any
-    stages {
-        stage('build') {
-            steps {
-                echo 'Clarusway_Way to Reinvent Yourself'
-                sh 'echo Integrating Jenkins Pipeline with GitHub Webhook using Jenkinsfile'
-            }
-        }
-    }
-}
+print("hello world")
